becd46782d0b224274e255392bec15e7d4b85156
Vika Zaichuk <viktoriia.zaichuk@gmail.com>
2023-02-13 16:53:52 +0100

    :recycle: code refactor

--- src/api/challenge.js
+++ src/api/challenge.js
@@ -3,11 +3,8 @@ import request from "./config";
 export const createChallenge = async (formData) => {
     const response = await request({ method: "post", url: "/challenge/create", data: formData})
     if (response.status === 201) {
-        console.log(response)
-        console.log("yess")
         return true
     } else {
-        console.log("noooo")
         return false
     }
 }
