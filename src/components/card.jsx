a8fc3fb4275cedb4a8f1c0f18f4f2fb9069eef30
Vika Zaichuk <viktoriia.zaichuk@gmail.com>
2023-03-07 10:39:05 +0100

    ✨challenges list - already played band + 💄 left menu ui fix

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Card = ({ challenge, demo }) => {
+const Card = ({ challenge, demo, createdByMe, hasPlayedByMe }) => {
+    const buttonText = createdByMe || hasPlayedByMe > 0 ? "Voir" : "Jouer";
+    const buttonClass = createdByMe || hasPlayedByMe > 0 ? "button card--btn created-by-me-btn" : "button card--btn";
+
     return (
         <div className="card">
             <div className="card--img">
@@ -12,7 +15,18 @@ const Card = ({ challenge, demo }) => {
                 {challenge.level === 1 ? <p>Facile</p> : challenge.level === 2 ? <p>Intermédiaire</p> : <p>Expert</p>}
             </div>
             <p className="card--txt">{challenge.description}</p>
-            <button className="button card--btn"><Link to={demo ? "/login" : `/challenge/${challenge.id}`}>Jouer</Link></button>
+            <button className={buttonClass}><Link to={demo ? "/login" : `/challenge/${challenge.id}`}>{buttonText}</Link></button>
+
+            {createdByMe && 
+                <div className="created-by-me">
+                    <spa>Ton challenge</spa>
+                </div>
+            }
+            {hasPlayedByMe > 0 && 
+                <div className="created-by-me">
+                    <spa>Déjà joué</spa>
+                </div>
+            }
         </div>
     )
 }
